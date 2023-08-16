7025f3fbf780f6870d251e6834cc96dc2483bc67
Gildson Caetano <gildsonrj@gmail.com>
2023-08-16 10:49:49 -0300

    Add files via upload

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,12 +2,12 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x85b1C92F0B9722083729375c117ED9aA614BCe23";
+export const contractConst = "0x6f1E7C7165cdAD8146FeA8E838fAe6421F548eBa";
 
 // The name of the chain your contract is deployed to.
 // Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
 // Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
-export const chainConst = "mumbai";
+export const chainConst = "polygon";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "#d4af37";
